a8393709c1d455886d1f21b8b6d5996935f6ffeb
nada <nada@nada-labs.net>
2014-02-23 18:10:10 -0800

    Added serial port output and string printing

---
+++ source/serial.c
@@ -0,0 +1,65 @@
+/**
+ *  JtagKnocker - JTAG finder and enumerator for STM32 dev boards
+ *  Copyright (C) 2014 Nathan Dyer
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/rcc.h>
+#include <strings.h>
+#include <stdarg.h>
+#include "serial.h"
+
+#define SERIAL_WRITE_BUFFER		(256)
+void serial_Init()
+{
+	//UART on PA2 (TX) and PA3 (RX) @ 115200,8,N,1	
+	rcc_periph_clock_enable(RCC_USART2);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO2| GPIO3);
+
+	usart_set_baudrate(USART2, 115200);
+	usart_set_databits(USART2, 8);
+	usart_set_stopbits(USART2, USART_STOPBITS_1);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_mode(USART2, USART_MODE_TX);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+	usart_enable(USART2);
+}
+
+void serial_Send(const char *buffer, const unsigned int len)
+{
+	unsigned int count;
+	const char *p = buffer;
+	for(count = 0; count < len; ++count)
+	{
+		usart_send_blocking(USART2, *p++);
+	}
+}
+
+int serial_Write(const char *fmt, ...)
+{
+	char buffer[SERIAL_WRITE_BUFFER];
+	int n;
+	va_list args;
+	va_start(args, fmt);
+	n = vsnprintf(buffer, SERIAL_WRITE_BUFFER, fmt, args);
+	if((n > 0) && (n < SERIAL_WRITE_BUFFER))
+	{
+		serial_Send(buffer, n);
+	}
+}
---
+++ source/serial.h
@@ -0,0 +1,25 @@
+/**
+ *  JtagKnocker - JTAG finder and enumerator for STM32 dev boards
+ *  Copyright (C) 2014 Nathan Dyer
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#if !defined(_SERIAL_H_)
+#define _SERIAL_H_
+
+void serial_Init();
+void serial_Send(const char *buffer, unsigned int len);
+int serial_Write(const char *fmt, ...);
+
+#endif
